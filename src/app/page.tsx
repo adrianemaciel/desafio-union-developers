fb7fb27791519ce8bcb2938c36323837bbf05545
Adriane Maciel <adrianemaciel.developer@gmail.com>
2025-02-20 15:52:21 -0300

    feat: add About section to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import Hero from "@/sections/Hero";
+import About from "@/sections/About";
 import "@/styles/globals.css";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <main>
         <Hero />
+        <About />
       </main>
     </>
   );
