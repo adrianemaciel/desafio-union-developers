349a810ccabafb16cfca752b7c39ae9b7c856665
Adriane Maciel <adrianemaciel.developer@gmail.com>
2025-03-06 17:14:35 -0300

    feat: implement responsive mobile menu and update header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,65 +2,75 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import MenuMobile from "./MenuMobile";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 w-full shadow-md z-50">
-      <div className="max-w-[1920px] mx-auto h-28 px-40 flex justify-between items-center bg-primary ">
+    <header className="max-w-[1920px] mx-auto fixed top-0 left-0 w-full z-50 bg-primary sm:px-6 md:px-8 lg:px-10 xl:px-16 2xl:px-40">
+      <div className="flex items-center justify-between h-16 md:h-20 lg:h-24">
         <Link href="/">
           <Image
             src="/logo.png"
             alt="Union Developers Logo"
-            width={263}
-            height={37}
+            width={300}
+            height={60}
+            className="w-auto h-10 md:h-8 lg:h-8 px-4"
           />
         </Link>
 
-        <nav className="flex gap-6 text-secondary text-xl font-bold ">
-          <Link
-            href="#sobre"
-            className=" hover:bg-gradient-to-r  to-gradient-to from-gradient-from hover:bg-clip-text hover:text-transparent ease-in-out "
-          >
-            Sobre
-          </Link>
-          <Link
-            href="#pilares"
-            className=" hover:bg-gradient-to-r  to-gradient-to from-gradient-from hover:bg-clip-text hover:text-transparent ease-in-out "
-          >
-            Pilares
-          </Link>
-          <Link
-            href="#beneficios"
-            className=" hover:bg-gradient-to-r  to-gradient-to from-gradient-from hover:bg-clip-text hover:text-transparent ease-in-out "
-          >
-            Benefícios
-          </Link>
-          <Link
-            href="#etapas"
-            className=" hover:bg-gradient-to-r  to-gradient-to from-gradient-from hover:bg-clip-text hover:text-transparent ease-in-out "
-          >
-            Etapas
-          </Link>
-          <Link
-            href="#depoimentos"
-            className=" hover:bg-gradient-to-r  to-gradient-to from-gradient-from hover:bg-clip-text hover:text-transparent ease-in-out "
-          >
-            Depoimentos
-          </Link>
-          <Link
-            href="#duvidas"
-            className=" hover:bg-gradient-to-r  to-gradient-to from-gradient-from hover:bg-clip-text hover:text-transparent ease-in-out "
-          >
-            Dúvidas
-          </Link>
-        </nav>
+        <div className="hidden lg:flex items-center justify-center gap-6 text-secondary text-lg font-bold">
+          <nav className="flex items-center gap-6">
+            <Link
+              href="#sobre"
+              className="hover:bg-gradient-to-r  to-gradient-to from-gradient-from hover:bg-clip-text hover:text-transparent ease-in-out "
+            >
+              Sobre
+            </Link>
+            <Link
+              href="#pilares"
+              className="hover:bg-gradient-to-r  to-gradient-to from-gradient-from hover:bg-clip-text hover:text-transparent ease-in-out "
+            >
+              Pilares
+            </Link>
+            <Link
+              href="#beneficios"
+              className="hover:bg-gradient-to-r  to-gradient-to from-gradient-from hover:bg-clip-text hover:text-transparent ease-in-out "
+            >
+              Benefícios
+            </Link>
+            <Link
+              href="#etapas"
+              className="hover:bg-gradient-to-r  to-gradient-to from-gradient-from hover:bg-clip-text hover:text-transparent ease-in-out "
+            >
+              Etapas
+            </Link>
+            <Link
+              href="#depoimentos"
+              className="hover:bg-gradient-to-r  to-gradient-to from-gradient-from hover:bg-clip-text hover:text-transparent ease-in-out "
+            >
+              Depoimentos
+            </Link>
+            <Link
+              href="#duvidas"
+              className="hover:bg-gradient-to-r  to-gradient-to from-gradient-from hover:bg-clip-text hover:text-transparent ease-in-out "
+            >
+              Dúvidas
+            </Link>
+          </nav>
+        </div>
 
-        <Link
-          href="#inscricao"
-          className=" inline-flex items-center justify-center w-72 h-14 text-white text-lg font-semibold rounded-md  ease-in-out bg-gradient-to-r from-gradient-from to-gradient-to shover:border-2"
-        >
-          <span className="relative z-10">Junte-se a nós</span>
-        </Link>
+        <div className="hidden lg:flex items-center ml-8">
+          <Link
+            href="#inscricao"
+            className="flex items-center justify-center bg-gradient-to-r to-gradient-to from-gradient-from text-white text-xl font-bold rounded-md transition-transform hover:scale-105 px-8 py-3 whitespace-nowrap"
+          >
+            Junte-se a nós
+          </Link>
+        </div>
+
+        <div className="lg:hidden">
+          <MenuMobile />
+        </div>
       </div>
     </header>
   );
